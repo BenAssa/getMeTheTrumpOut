e09bbb616f6a7b0eba96e673335e6b6920b85495
Ben <yaearb@gmail.com>
2016-10-21 20:38:45 -0700

    poppy kind of works

---
+++ app/controllers/Poppy.java
@@ -0,0 +1,8 @@
+package controllers;
+
+/**
+ * Created by ben on 10/21/16.
+ */
+public class Poppy
+{
+}
---
+++ app/models/AlchemyModel.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * Created by ben on 10/21/16.
+ */
+public class alchemy
+{
+}
